dac5e97df6fe42b49de46628dc95a445ab8f9e79
Jack <jaxanimations@gmail.com>
2020-05-09 17:30:46 -0500

    Added continuous formatting in new tabs
    - Added continous formatting across tabs when using the new tab button
    - Fixed a bug where the color pickers and font size picker would sometimes default to black and blank, respectively

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,10 @@ const DEFAULT_TEXT_COLOR = "#00FF00";
 const DEFAULT_BACKGROUND_COLOR = "#000000";
 const DEFAULT_FONT_SIZE = 20;
 
+const TEXT_COLOR_PARAMETER = "txt-col";
+const BACKGROUND_COLOR_PARAMETER = "bg-col";
+const FONT_SIZE_PARAMETER = "fnt-size";
+
 const SMALLEST_FONT = 1;
 const LARGEST_FONT = 200;
 
@@ -17,17 +21,40 @@ function initialize()
     var body = document.getElementsByTagName("body")[0];
     var fontSizePicker = document.getElementById("font-size-picker");
 
+    //Get URL parameters
+    const URL_QUERY = window.location.search;
+    const URL_PARAMETERS = new URLSearchParams(URL_QUERY);
+
     //First, check for first load
     if (localStorage.getItem("hasCodeRunBefore") === null)
     {
-        //Set text color picker default value
-        textColorPicker.value = DEFAULT_TEXT_COLOR;  //Default color is lime
+        //If there are no paramters (indicated by the presense of no text-color parameter), load default values
+        if (!URL_PARAMETERS.has(TEXT_COLOR_PARAMETER))
+        {
+            //Set text color picker default value
+            textColorPicker.value = DEFAULT_TEXT_COLOR;  //Default color is lime
 
-        //Set background color picker default value
-        backgroundColorPicker.value = DEFAULT_BACKGROUND_COLOR;
+            //Set background color picker default value
+            backgroundColorPicker.value = DEFAULT_BACKGROUND_COLOR;
 
-        //Set text size color picker default value
-        fontSizePicker.value = DEFAULT_FONT_SIZE;
+            //Set text size color picker default value
+            fontSizePicker.value = DEFAULT_FONT_SIZE;
+        }
+        //If ther are parameters, read parameter values and assign settings
+        else
+        {
+            //Get values from URL parameters
+            const TEXT_COLOR = URL_PARAMETERS.get(TEXT_COLOR_PARAMETER);
+            const BACKGROUND_COLOR = URL_PARAMETERS.get(BACKGROUND_COLOR_PARAMETER);
+            const FONT_SIZE = URL_PARAMETERS.get(FONT_SIZE_PARAMETER);
+
+            //Assign page settings
+            //Set the settings for the text areas
+            textArea.style.color = TEXT_COLOR;
+            textArea.style.backgroundColor = BACKGROUND_COLOR;
+            body.style.backgroundColor = BACKGROUND_COLOR;
+            textArea.style.fontSize = FONT_SIZE + "px";
+        }
 
         //Set has run before flag
         localStorage.setItem("hasCodeRunBefore", true);
@@ -44,7 +71,12 @@ function initialize()
         body.style.backgroundColor = backgroundColorPicker.value;
         textArea.style.fontSize = fontSizePicker.value + "px";
     }
-    
+}
+
+function confirmLeave()
+{
+    var confirmStatus = confirm("Are you sure you want to leave the page? Unsaved data will be lost.");
+    return confirmStatus;
 }
 
 /*
@@ -52,7 +84,20 @@ Opens a new Net Pad tab.
 */
 function newTab()
 {
-    window.open("https://jackcsheehan.github.io/net-pad/");
+    //Gather data from web page
+    const TEXT_COLOR = document.getElementById("text-color-picker").value;
+    const BACKGROUND_COLOR = document.getElementById("background-color-picker").value;
+    const FONT_SIZE = document.getElementById("font-size-picker").value;
+
+    //Create a custom query string that indicates the user's format settings
+    const QUERY_STRING = "?" + TEXT_COLOR_PARAMETER + "=" + TEXT_COLOR + "&"
+                             + BACKGROUND_COLOR_PARAMETER + "=" + BACKGROUND_COLOR + "&"
+                             + FONT_SIZE_PARAMETER + "=" + FONT_SIZE;
+
+    //Create URL based on custom query string
+    const URL = "https://jackcsheehan.github.io/net-pad/" + QUERY_STRING;
+
+    window.open(URL);
 }
 
 /*
@@ -61,8 +106,8 @@ This function sets the text color of the main text area to the color set in the 
 function setTextColor()
 {
     //Get the color selected in the color picker
-    var colorPicker = document.getElementById("text-color-picker");
-    var selectedColor = colorPicker.value;
+    const COLOR_PICKER = document.getElementById("text-color-picker");
+    var selectedColor = COLOR_PICKER.value;
     
     //Set the text color of the main text area to that color
     var textArea = document.getElementById("text-area");
@@ -75,8 +120,8 @@ Sets the background color of main text area based on the color in the color pick
 function setBackgroundColor()
 {
     //Get the color selected in the color picker
-    var colorPicker = document.getElementById("background-color-picker");
-    var selectedColor = colorPicker.value;
+    const COLOR_PICKER = document.getElementById("background-color-picker");
+    var selectedColor = COLOR_PICKER.value;
     
     //Set the background color of the main text area to that color
     var textArea = document.getElementById("text-area");
